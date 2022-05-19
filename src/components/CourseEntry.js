2560d7cdd21547009642820f4453224c84af92b3
anjalireghunath <anjalireghu98@gmail.com>
2022-05-19 11:15:50 +0530

    course

--- src/components/CourseEntry.js
+++ src/components/CourseEntry.js
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const CourseEntry = () => {
+    var [title,setTitle]=useState("")
+    var [duration,setDuration]=useState("")
+    var [description,setDescription]=useState("")
+    var [venue,setVenue]=useState("")
+    var [date,setDate]=useState("")
+    const subData=()=>{
+        const data={"title":title,"duration":duration,"description":description,"venue":venue,"date":date}
+        console.log(data)
+    }
   return (
     <div>
 
@@ -10,26 +19,26 @@ const CourseEntry = () => {
             <div class="row">
                 <div class="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                     <label for="" class="form-label">Course Title</label>
-                    <input type="text" class="form-control"/>
+                    <input onChange={(a)=>{setTitle(a.target.value)}} type="text" class="form-control"/>
                 </div>
                 <div class="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                     <label for="" class="form-label">Duration</label>
-                    <input type="text" class="form-control"/>
+                    <input onChange={(a)=>{setDuration(a.target.value)}} type="text" class="form-control"/>
                 </div>
                 <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                     <label for="" class="form-label">Description</label>
-                    <textarea name="" id="" cols="30" rows="10" class="form-control"></textarea>
+                    <textarea onChange={(a)=>{setDescription(a.target.value)}} name="" id="" cols="30" rows="10" class="form-control"></textarea>
                 </div>
                 <div class="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                     <label for="" class="form-label">Venue</label>
-                    <input type="text" class="form-control"/>
+                    <input onChange={(a)=>{setVenue(a.target.value)}} type="text" class="form-control"/>
                 </div>
                 <div class="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                     <label for="" class="form-label">Date</label> 
-                    <input type="date" name="" id="" class="form-control"/>
+                    <input onChange={(a)=>{setDate(a.target.value)}} type="date" name="" id="" class="form-control"/>
                 </div>
                 <div class="col  col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                    <button class="btn btn-success">REGISTER</button>
+                    <button onClick={subData} class="btn btn-success">REGISTER</button>
                 </div>
             </div>
         </div>
