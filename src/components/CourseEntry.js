ff9fc18ed6f7c98d77692dd0d24da5c56b6972a2
anjalireghunath <anjalireghu98@gmail.com>
2022-05-19 11:27:28 +0530

    course

--- src/components/CourseEntry.js
+++ src/components/CourseEntry.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import Header from './Header'
 
 const CourseEntry = () => {
     var [title,setTitle]=useState("")
@@ -12,7 +13,7 @@ const CourseEntry = () => {
     }
   return (
     <div>
-
+<Header/>
 <div class="container">
     <div class="row">
         <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
